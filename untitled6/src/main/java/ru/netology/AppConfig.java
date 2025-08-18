605e36b2222c88409e643e32e3dc001720c7df49
vitenkaaaa <puzikovv86@gmail.com>
2025-08-18 10:11:53 +0400

    Add files via upload

---
+++ untitled6/src/main/java/ru/netology/AppConfig.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public PostRepository postRepository() {
+        return new PostRepository();
+    }
+
+    @Bean
+    public PostService postService(PostRepository repository) {
+        return new PostService(repository);
+    }
+
+    @Bean
+    public PostController postController(PostService service) {
+        return new PostController(service);
+    }
+}
